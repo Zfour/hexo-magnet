fcb49e7686da48eccebfe8021e435243ea332e0b
Qin <2472963374@qq.com>
2023-10-14 23:43:07 +0800

    实现「更多目录」链接自定义
    主要解决[# Issues 5](https://github.com/Zfour/hexo-magnet/issues/5)。增加一个可配置的选项categories_dir

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ hexo.extend.filter.register('after_generate',function() {
         var tag_list = hexo.locals.get('tags').data;
         if(hexo.config.magnet.type ==='categories'){
             data_list = categories_list
-            load_more_href = '/categories'
+            load_more_href = hexo.config.url + hexo.config.magnet.categories_dir // 自定义目录的修改
         }else if (hexo.config.magnet.type ==='tags'){
             data_list = tag_list
             load_more_href = '/tags'
